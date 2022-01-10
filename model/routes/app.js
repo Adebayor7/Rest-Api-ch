8b95d8c190641e0f74498dc898fdd27bda7bf215
Adebayor <idrisadeebayorr12@gmail.com>
2022-01-10 11:26:09 +0100

    changes made

--- model/routes/app.js
+++ model/routes/app.js
@@ -1,23 +1,9 @@
-require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
 const router = express.Router;
 const User = require("../models/user");
 
-const app = express();
 
-mongoose.connect(process.env.MONGO_URI)
-//mongoose.Promise = global.Promise;
-// app.use(express.static("public"));
-//app.use("/api", require("./routes/api"));
-
-app.get("/api", (req, res) => {
-  res.send("it is working");
-});
-
-app.listen(process.env.port || 3000, function () {
-  console.log("now listening for requests");
-});
+//get all users from db
 router.get("/users", (req, res, next) => {
   User.find({})
     .then(function (users) {
@@ -42,11 +28,16 @@ router.put("/users/:id", (req, res, next ) => {
     .then(function (users){
   res.send(User)
 }
-)});
+);
 
 
 //delete a user from db
 router.delete("/users/:id", (req, res, next) => {
   User.findOneAndDelete({_id: req.params.id}).then(function(user){
         res.send(user);
-})}); 
+});
+
+
+module.exports = router
+});
+});
